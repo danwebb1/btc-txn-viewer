6705161d49651c6243dbfdca8f85d6802bf713b0
danwebb1 <dan.webb8706@gmail.com>
2025-01-26 11:31:21 -0600

    add typescript, tailwind, dotenv, and env vars

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { createContext, useContext, useEffect, useState } from 'react';
-import { auth } from '../config/firebase';
+import { auth } from '../config/firebase.ts';
 import { onAuthStateChanged } from 'firebase/auth';
 
 const AuthContext = createContext({});
